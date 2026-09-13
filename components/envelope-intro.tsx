e9d0e94fe6d217bf1466b73c797e5bb2fe96de40
gamindu1997 <157443100+gamindu1997@users.noreply.github.com>
2026-09-13 12:11:01 +0530

    Feature: Video Implementation 6

--- components/envelope-intro.tsx
+++ components/envelope-intro.tsx
@@ -12,7 +12,7 @@ export function EnvelopeIntro() {
     // After 10 seconds, transition to language selector
     const timer = setTimeout(() => {
       setShowLanguageSelector(true);
-    }, 13000);
+    }, 12000);
 
     return () => clearTimeout(timer);
   }, []);
